32b280499bdd27a87e36cc9e0fe97a65f2c20088
Mastermind <86709307+Mastermindx33@users.noreply.github.com>
2021-07-26 20:59:17 +0530

    __init__scraper

---
+++ website-scraper.js
@@ -0,0 +1,21 @@
+// Downloads all the crawlable files of example.com.
+// The files are saved in the same structure as the structure of the website, by using the `bySiteStructure` filenameGenerator.
+// Links to other websites are filtered out by the urlFilter
+const scrape = require('website-scraper');
+const websiteUrl = 'https://jacekjeznach.com/';
+
+scrape({
+    urls: [websiteUrl],
+    urlFilter: function (url) {
+        return url.indexOf(websiteUrl) === 0;
+    },
+    recursive: true,
+    maxDepth: 50,
+    prettifyUrls: true,
+    filenameGenerator: 'bySiteStructure',
+    directory: './Downloaded-websites'
+}).then((data) => {
+    console.log("Entire website succesfully downloaded");
+}).catch((err) => {
+    console.log("An error ocurred", err);
+});
