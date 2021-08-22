52a93ef9c06aaed5eab5a4964ae2189e1e91f3e9
Mastermind <mastermindx33@gmail.com>
2021-08-22 17:53:06 +0530

    Update website-scraper.js

--- website-scraper.js
+++ website-scraper.js
@@ -2,7 +2,7 @@
 // The files are saved in the same structure as the structure of the website, by using the `bySiteStructure` filenameGenerator.
 // Links to other websites are filtered out by the urlFilter
 const scrape = require('website-scraper');
-const websiteUrl = 'https://jacekjeznach.com/';
+const websiteUrl = '# URL2Scrape';
 
 scrape({
     urls: [websiteUrl],
